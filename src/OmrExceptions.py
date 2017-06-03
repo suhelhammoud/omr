83734832d8183ce2774e8ec3b12daab0acaa8f37
suhel hammoud <suhel.hammoud@gmail.com>
2017-06-03 12:44:12 +0300

    add intellij configurations

---
+++ src/OmrExceptions.py
@@ -0,0 +1,13 @@
+class NoVertexError(Exception):
+    """
+    Could not find vertex
+    """
+    pass
+
+
+class NoBorderDetectedError(Exception):
+    """
+    Could not find four vertices
+    """
+    pass
+
